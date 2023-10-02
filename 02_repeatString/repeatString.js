a564bd4063bf84134fb9e923c80246146847631d
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-02 15:49:11 -0400

    Implement solution for 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // Handle expected errors
+    if (num < 0) return 'ERROR';
 
+    const stringArr = [];
+    for (let i=0; i<num; i++) {
+        stringArr.push(string);
+    }
+    return stringArr.join('');
 };
 
 // Do not edit below this line
