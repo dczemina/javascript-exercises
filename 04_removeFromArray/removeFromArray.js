b1db11a0f535912154c0fb3b6672076cc6e8a469
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-02 15:49:26 -0400

    Implement solution for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let returnArr = [...arr];
+    for (let i=1; i<arguments.length; i++) {
+        returnArr = returnArr.filter(element => element !== arguments[i]);
+    }
+    return returnArr;
 };
 
 // Do not edit below this line
