6341192b3fb74284234cb3d93fe28b57fdb9d278
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-02 15:49:53 -0400

    Implement solution for 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if (isInvalidType(tempF)) return 'ERROR';
+  return +((tempF - 32) * (5/9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if (isInvalidType(tempC)) return 'ERROR';
+  return +(tempC * 9/5 + 32).toFixed(1);
 };
+
+function isInvalidType(val) {
+  return (typeof val !== 'number');
+}
 
 // Do not edit below this line
 module.exports = {
