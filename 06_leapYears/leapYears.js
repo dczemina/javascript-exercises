7c0d25208992b55a60ce5f5234396e0d81e12a15
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-02 15:49:44 -0400

    Implement solution for 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // not part of test spec, but keeping to be consistent with other exercises
+    if (typeof year !== 'number') return 'ERROR';
 
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 !== 0)
+            return false;
+        return true;
+    }
+    return false; // default
 };
 
 // Do not edit below this line
