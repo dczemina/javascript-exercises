5d05c94bda00b5bc7df49c38aa7f1bd2e14d84d6
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-02 15:49:33 -0400

    Implement solution for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Handle invalid arguments
+    if (num1 < 0 || num2 < 0) return 'ERROR'
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
 
+    let returnVal = 0;
+    // If num1 > num2, swap order in loop
+    for (let i=(num1 > num2? num2 : num1); i<=(num1 > num2? num1 : num2); i++) {
+        returnVal += i;
+    }
+    return returnVal;
 };
 
 // Do not edit below this line
