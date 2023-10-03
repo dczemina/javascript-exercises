50976866134a467eee6b3b2a1ca636cc4c640336
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-03 19:48:41 -0400

    Implement solution for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function(a, b) {
+  if (isInvalidType(a) || isInvalidType(b)) return 'ERROR';
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  if (isInvalidType(a) || isInvalidType(b)) return 'ERROR';
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(el => {
+    if (isInvalidType(el)) {
+      return 'ERROR'
+    }
+    sum += el;
+  })
+  return sum;	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product;
+  arr.forEach(el => {
+    if (isInvalidType(el)) {
+      return 'ERROR'
+    }
+    if (!product) {
+      product = el;
+    } else {
+      product *= el;
+    }
+  })
+  return product;	
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (isInvalidType(a) || isInvalidType(b)) return 'ERROR';
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (isInvalidType(a)) return 'ERROR';
+	if (a < 0) return 'ERROR';
+  if (a===1 || a===0) return 1;
+  return a * factorial(a-1);
 };
+
+function isInvalidType(val) {
+  return (typeof val !== 'number');
+}
 
 // Do not edit below this line
 module.exports = {
