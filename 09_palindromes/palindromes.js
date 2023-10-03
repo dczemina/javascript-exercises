3afedb42a834a0104f0bee7987486db50367a508
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-03 19:57:06 -0400

    Implement solution for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regex = /[^A-Za-z0-9]*/g;
+  const cleanStr = str.replace(regex, '').toUpperCase();
+  const cleanStrArr = cleanStr.split('');
+  const cleanStrRev = cleanStrArr.reverse().join('')
+  return cleanStr === cleanStrRev;
 };
 
 // Do not edit below this line
