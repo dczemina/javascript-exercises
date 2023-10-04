30ba24c5fac2f25b690582d47828c5fe894b6b42
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-03 20:24:37 -0400

    Implement solution for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if ((typeof n !== 'number' && !+n) || n < 0) return 'OOPS';
 
+    const sequence = [];
+    for (let f=0; f<n; f++) {
+        if (f<2) {
+            sequence.push(1);
+        } else {
+            sequence.push(sequence[f-2] + sequence[f-1])
+        }
+    }
+    return sequence[sequence.length-1]
 };
 
 // Do not edit below this line
